87eae27664682f9f4a8fb5aa5042018e7fc90fee
Konstantin Vasilev <burmuley@gmail.com>
2023-05-13 01:54:15 -0700

    * Added Apache license

--- internal/processor/dapr.go
+++ internal/processor/dapr.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023. Konstantin Vasilev (burmuley@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package processor
 
 import (
@@ -8,11 +24,6 @@ type Dapr struct {
 	subscriberUrl string
 	method        string
 	timeout       int64
-}
-
-func NewDapr(config map[string]string) (*Dapr, error) {
-	// TODO: implement me!
-	return nil, nil
 }
 
 func (d *Dapr) Run(msg queue.Message) error {
